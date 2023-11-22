7ccffb640c9852e255214822bac3147835348fea
enrico420 <enrico.vieira95@yahoo.com.br>
2023-11-21 22:42:45 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipalVue from './components/ConteudoPrincipal.vue';
+import Rodape from './components/Rodape.vue';
+
+export default {
+  components: { Banner, ConteudoPrincipalVue, Rodape }
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipalVue />
+  <Rodape />
+</template>
